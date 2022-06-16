e675a1bee476941eb6652c21849dd9d9df1ae181
Henrique Ferraz de Oliveira <henrique.oliveira@calindra.com.br>
2022-06-16 14:26:35 -0300

    feat: iniciando servidor

--- server.js
+++ server.js
@@ -3,14 +3,6 @@ const fs = require('fs');
 const path = require('path');
 
 
-const server = net.createServer((socket) => {
-    socket.on('connect', (socket) => {
-        socket.on('data', (data) => {
-            console.log(`cliente escreveu: ${data}` );
-        })
-    })
-});
-
-server.listen(8080, () => {
-    console.log("servidor rodando na porta 8080");
-});
+http.createServer((req, res) => {
+    console.log('req', req.url);
+})
