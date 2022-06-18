a1fd097c1625f5de16a66d272b919e421ef59e1f
Henrique Ferraz de Oliveira <henrique.oliveira@calindra.com.br>
2022-06-18 11:30:17 -0300

    feat: modificando leitura de arquivo estatico

--- server.js
+++ server.js
@@ -1,53 +1,44 @@
 const http = require('http');
 const fs = require('fs');
-const path = require('path');
+const path = require('path')
+const url = require('url')
 
 
 http.createServer((req, res) => {
     console.log('req', req.url);
 
-    let filePath = '.' + req.url;
-    if( filePath == './' ) {
-        filePath = './index.html';
-    }
+    let parseUrl = url.parser(req.url);
+    let pathName = `${parseUrl.pathName}`;
+    let extensions = path.parser(pathName).extensions;
 
-    let extname = String(path.extrame(filePath)).toLowerCase();
-    let mimeType = {
+    let mapMimeType = {
         '.html': 'text/html',
-        '.js': 'text/javascript',
-        '.css': 'text/css',
         '.json': 'application/json',
         '.png': 'image/png',
         '.jpg': 'image/jpg',
-        '.gif': 'image/gif',
-        '.svg': 'image/svg+xml',
-        '.wav': 'audio/wav',
-        '.mp4': 'video/mp4',
-        '.woff': 'application/font-woff',
-        '.ttf': 'application/font-ttf',
-        '.eot': 'application/vnd.ms-fontobject',
-        '.otf': 'application/font-otf',
-        '.wasm': 'application/wasm'
+        '.pdf' : 'application/pdf',
+        '.doc' : 'application/doc'
     }
 
-    let contentType = mimeTypes[extname] || 'application/octet=stream';
-
-    fs.readFile(filePath, (error, content) => {
-        if ( error ) {
-            if( error.code == 'ENOENT' ) {
-                fs.readFile('./404.html', (error, content) => {
-                    res.writeHead(404, { 'Content-Type' : 'text/html' });
-                    res.end( content, 'utf-8' );
-                })
-            } else {
-                res.writeHead(500);
-                res.end(`Desculpe, contate o responsavel pelo servidor para informar o erro ${error.code} ...` )
-            }
-        } else {
-            res.writeHead(500);
-            res.end(content, 'utf-8');
+    fs.readFile(pathName, 'utf-8', (exist) => {
+        if(!exist) {
+          res.statusCode = 404;
+          res.end(`File ${pathName} not found!`);
+          return;
         }
+    
+        if (fs.statSync(pathName).isDirectory()) pathName += '/index' + ext;
+    
+        fs.readFile(pathName, 'utf-8', (err, data) => {
+          if(err){
+            res.statusCode = 500;
+            res.end(`Error getting the file: ${err}.`);
+          } else {
+            res.setHeader('Content-type', map[ext] || 'text/plain' );
+            res.end(data);
+          }
+        });
     });
-
-}).listen(8080);
-console.log("Servidor executando em http://127.0.0.1:8080/");
+}).listen( 8080, () => {
+    console.log("Servidor executando em http://127.0.0.1:8080/");
+});
